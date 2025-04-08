0e5e3d59d80381fe5d28687a8e3e15e34a6312e6
pagrawal15 <priyankaagrawal2611@gmail.com>
2025-04-08 22:13:58 +0000

    changes

---
+++ short_string1.py
@@ -0,0 +1,23 @@
+def short_string(s):
+    prev = s[0]
+    cnt = 1
+    result = []
+
+    for i in range(1,len(S)):
+        if s[i] == prev:
+            cnt+=1
+        else:
+            result.append(prev+str(cnt))
+            prev = s[i]
+            cnt = 1
+    result.append(prev + str(cnt))
+
+    compressed = "".join(result)
+    return compressed if len(compressed) <len(s) else s
+
+# test cases
+if __init__ == "__main__":
+    print(short_string("aaabbcccaaa"))  # a3b2c3a3
+    print(short_string("abc"))         # abc
+    print(short_string("a"))           # a
+    print(short_string(""))            # (empty string)
