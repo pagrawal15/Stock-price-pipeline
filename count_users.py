a9d2c181da1bd966faf077eacf87e7c32d98cccd
pagrawal15 <priyankaagrawal2611@gmail.com>
2025-04-09 17:49:16 +0000

    new q

---
+++ count_users.py
@@ -0,0 +1,15 @@
+def count_users(emails):
+    domain = emails.split('@')
+    reurn domain
+
+
+# Test. cases
+
+if __name__ == "__main__":
+    emails = [
+    "alice@meta.com",
+    "bob@openai.com",
+    "carol@meta.com",
+    "dave@openai.com",
+    "eve@amazon.com"
+]
